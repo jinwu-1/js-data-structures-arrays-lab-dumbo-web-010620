69f05a8bfd802665f2d9c941f5a6e56ce5a762c4
Jin Wu <jinn.wuu@gmail.com>
2020-02-16 21:35:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 const drivers = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendDriver(name){
-  drivers.push(name)
+  drivers.push(name);
 }
 
 function destructivelyPrependDriver(name){
-  drivers.unshift(name)
+  drivers.unshift(name);
 }
 
